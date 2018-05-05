4fafe7c961ce79f808db02b47c7ac79293d8959b
andrei <claudiutoader98@yahoo.com>
2018-05-05 17:25:04 +0300

    fist commit

---
+++ task/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView, ListView
+from task.models import TaskItems
+
+class Index(TemplateView):
+    template_name = "base.html"
+
+class TaskListView(ListView):
+    template_name = "task/task_list.html"
+    queryset = TaskItems.objects.all()
